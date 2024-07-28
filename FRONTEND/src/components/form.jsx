4dfd3ff429dc25ca3e9ea3d4ea449015b6d789f9
Joaquin Lopez <Joaquinlopez21@outlook.es>
2024-07-28 04:12:34 -0300

    new changes on form

--- FRONTEND/src/components/form.jsx
+++ FRONTEND/src/components/form.jsx
@@ -4,18 +4,47 @@ function Form() {
   const [formData, setFormData] = useState({
     ign: "",
     dfprofiler: "",
+    country: "",
+    lenguage: "",
   });
   return (
-    <form>
-      <div>
-        <label htmlFor="name">IGN:</label>
-        <input type="text" id="name" name="name" />
+    <form className="text-black">
+      <div className="p-4">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="IGN"
+          className="text-center p-1"
+        />
       </div>
-      <div>
-        <label htmlFor="dfprofiler">DF PROFILER link:</label>
-        <input type="text" name="dfprofiler" />
+      <div className="p-4">
+        <input
+          type="text"
+          name="dfprofiler"
+          placeholder="DF Profiler Link"
+          className="p-1 text-center w-[400px]"
+        />
       </div>
-      <button type="submit" className="bg-white text-black p-1 rounded-sm mt-5">
+      <div className="p-4">
+        <input
+          type="text"
+          name="country"
+          placeholder="Pais"
+          className="p-1 text-center"
+        />
+      </div>
+      <div className="p-4">
+        <input
+          type="text"
+          name="lenguaje"
+          placeholder="Lenguaje/s"
+          className="p-1 text-center"
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-36 h-12 text-2xl font-ysabeau font-bold bg-white text-black p-1 rounded-sm mt-5 hover:bg-black hover:text-white transition-all duration-500">
         Enviar
       </button>
     </form>

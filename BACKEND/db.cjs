9d5cdcb1b00bd7cc8e0ecc3c8a6c41c85aceb1a2
Joaquin Lopez <Joaquinlopez21@outlook.es>
2024-07-29 18:56:04 -0300

    build

--- BACKEND/db.cjs
+++ BACKEND/db.cjs
@@ -3,9 +3,9 @@ let mysql = require("mysql2");
 
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
 
